e903b38ce22bff54a42cfade462d7c9fee8d88ed
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2022-01-10 11:08:04 -0300

    testando business como admin

--- test.js
+++ test.js
@@ -1,24 +1,59 @@
 require('chromedriver');
 const assert = require('assert');
-const {Builder, Key, By, until} = require('selenium-webdriver');
+const { Builder, Key, By, until } = require('selenium-webdriver');
 
-describe('Checkout Google.com', function () {
+describe('Testar Business como Admin', function() {
     let driver;
-
     before(async function() {
         driver = await new Builder().forBrowser('chrome').build();
     });
 
-    it('Search on Google', async function() {
-        await driver.get('https://google.com');
-        await driver.findElement(By.name('q')).click();
-        await driver.findElement(By.name('q')).sendKeys('dalenguyen', Key.RETURN);
-        // await driver.wait(until.titleIs('dalenguyen - Google Search'), 1000);
-        await driver.wait(until.elementLocated(By.id('rcnt')), 10000);
-        await driver.findElement(By.name('q')).clear();
+    it('Logar', async function() {
+        await driver.get('http://localhost:3000/login');
+        await driver.findElement(By.id('email')).sendKeys('admin@email.com');
+        await driver.findElement(By.id('password')).sendKeys('123123');
+        await driver.findElement(By.id('login-button')).click();
+        await driver.wait(until.elementLocated(By.id('organization-screen')), 2000);
+    });
 
-        let title = await driver.getTitle();
-        assert.equal(title, 'dalenguyen - Google Search');              
+    // it('Adicionar Organização', async function() {
+    //     await driver.findElement(By.id('add-organization-button')).click();
+    //     await driver.findElement(By.id('nome')).sendKeys('orgteste2');
+    //     await driver.findElement(By.id('email')).sendKeys('orgteste2@email.com');
+    //     await driver.findElement(By.id('endereco')).sendKeys('rua teste');
+    //     await driver.findElement(By.id('incluir')).click();
+
+    //     await driver.wait(until.elementLocated(By.id('organization-screen')), 2000);
+    // });
+
+    it('Verificar Vacinas', async function() {
+        await driver.get('http://localhost:3000/vaccines');
+        await driver.wait(until.elementLocated(By.id('vaccines-screen')), 2000);
+    });
+
+    // it('Adicionar Usuário', async function() {
+    //     await driver.get('http://localhost:3000/users');
+    //     await driver.findElement(By.id('add-user-button')).click();
+    //     await driver.findElement(By.id('tipo')).sendKeys('Administrativo');
+    //     await driver.sleep(1000);
+    //     await driver.findElement(By.id('email')).sendKeys('userteste2@email.com');
+    //     await driver.findElement(By.id('continuar')).click();
+    //     await driver.wait(until.elementLocated(By.id('users-screen')), 2000);
+    // });
+
+    it('Verificar Funcionalidades', async function() {
+        await driver.get('http://localhost:3000/features');
+        await driver.wait(until.elementLocated(By.id('features-screen')), 2000);
+    });
+
+    it('Verificar FAQ', async function() {
+        await driver.get('http://localhost:3000/frequent-questions');
+        await driver.wait(until.elementLocated(By.id('faq-screen')), 2000);
+    });
+
+    it('Deslogar', async function() {
+        await driver.findElement(By.id('logout-button')).click();
+        await driver.wait(until.elementLocated(By.id('login-screen')), 2000);
     });
 
     after(() => driver && driver.quit());
